65f1f834ee309dc7120b9ef5be6c5b0ab112ecb6
Ubuntu <ubuntu@ip-172-31-26-92.us-east-2.compute.internal>
2019-07-08 11:29:49 +0000

    f

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ var authRoutes = require("./routes/auth"),
     feedbackRoutes = require("./routes/feedback"),
     userRoutes = require("./routes/user");
 
-// mongoose.connect("mongodb+srv://jaspreet:singh@cluster0-aw4yr.mongodb.net/QuestionnaireV2?retryWrites=true&w=majority", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
+mongoose.connect("mongodb+srv://jaspreet:singh@cluster0-aw4yr.mongodb.net/QuestionnaireV2?retryWrites=true&w=majority", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
 
-mongoose.connect("mongodb://localhost/Questionnaire", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
+// mongoose.connect("mongodb://localhost/Questionnaire", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
 
 app.use(flash());
 app.set("view engine", "ejs");
