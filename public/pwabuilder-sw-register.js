f3194322e2361fec69adc1403d54086cbcebff21
Ubuntu <ubuntu@ip-172-31-26-92.us-east-2.compute.internal>
2019-07-18 08:24:15 +0000

    2

--- public/pwabuilder-sw-register.js
+++ public/pwabuilder-sw-register.js
@@ -1,4 +1,4 @@
-// This is the service worker with the Advanced caching
+// This is the service worker with the Cache-first network
 
 // Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 
