00cead29c92ce20d8955785a10a40a2985145f7a
Ubuntu <ubuntu@ip-172-31-26-92.us-east-2.compute.internal>
2019-07-19 20:58:04 +0000

    nightly

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var authRoutes = require("./routes/auth"),
 mongoose.connect("mongodb+srv://jaspreet:singh@cluster0-aw4yr.mongodb.net/Kollect?retryWrites=true&w=majority", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, autoIndex: true });
 
 // mongoose.connect("mongodb://localhost/Questionnaire", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
-
+app.enable("trust proxy");
 app.use(flash());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
