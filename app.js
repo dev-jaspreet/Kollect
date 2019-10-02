301a7ab4b50f94b7d80758f23a6b4e3d540714b9
Ubuntu <ubuntu@ip-172-31-26-92.us-east-2.compute.internal>
2019-10-02 13:18:24 +0000

    sepetemver v2

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var authRoutes = require("./routes/auth"),
 
 
 
-mongoose.connect("mongodb+srv://jaspreet:singh@cluster0-aw4yr.mongodb.net/Kollect?retryWrites=true&w=majority", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, autoIndex: true });
+mongoose.connect("mongodb+srv://jaspreet:singh@cluster0-aw4yr.mongodb.net/Kollect?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, autoIndex: true });
 
 // mongoose.connect("mongodb://localhost/Questionnaire", { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
